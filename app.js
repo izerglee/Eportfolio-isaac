d3b66ce9b58c68451802b5bd0cfc630979340377
Isaac lee <izerglee@gmail.com>
2023-10-25 00:33:06 +0800

    adding emailjs

---
+++ app.js
@@ -0,0 +1,11 @@
+template_q4qaj5a
+service_usg0ybt
+
+<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">
+</script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init("oUcotxfYz-zac6T2O");
+   })();
+</script>
